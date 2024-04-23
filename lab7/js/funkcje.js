5a3704c9d69c93c387405e797501a73f92e90f20
Kacper Wiącek <xxxwiacekxxx@gmail.com>
2024-04-23 12:02:36 +0200

    lab7
     (nw czy do poprawki)

--- lab7/js/funkcje.js
+++ lab7/js/funkcje.js
@@ -6,45 +6,43 @@ function pokaz(id){
     case 4: tresc += pokazKontakt();break;
     default: tresc += pokazO();
   }
-  //pobierz element o wskazanym id i ustaw jego nową zawartość:
   document.getElementById('blok').innerHTML = tresc;
 }
 function pokazO(){
   var tresc ='<h2><br />Pierwsze kroki</h2> ';
-  //operator += uzupełnia łańcuch kolejną porcją znaków:
   tresc += '<p> W aplikacjach typu SPA ......</p>'+
   '<p class="srodek"><img src="images/baner.jpg" alt="Zdjęcie" /></p>'+
   '<article><h2>Wady SPA</h2><p>'+
   ' Czas wytworzenia oraz nakład pracy ... </p></article>';
-  //przekaż wynik – gotową zawartość – do miejsca wywołania funkcji:
   return tresc;
 }
 function pokazGalerie(){
   var tresc='<h2><br />Moja galeria</h2>';
   tresc+=' <div class="galeria">';
-  //wygeneruj kod HTML z obrazami za pomocą pętli for:
   for(i=1;i<=10;i++){
-    tresc+='<div class="slajd"> <img ... /></div>';
+    tresc+='<div class="slajd"> <img src="zdjecia/obraz' + i + '.jpg"/></div>';
   }
   return tresc+'</div>';
 }
 function pokazKontakt(){
   var tresc='<h2><br />Kontakt</h2>';
-  //uzupełnij treść:
-  // tresc+= ...
+  tresc+= '<h1>Kacper Wiącek</h1>';
+  tresc+= '<h1><a href="github.com/DScraftPL">Github!</a></h1>';
   return tresc;
 }
 function pokazPost(){
-  var tresc = "<h1>to jest post!</h1>"
+  var tresc = "<h1>to jest post!</h1>";
+  tresc+='<h1><form>Imie: <input type="text" id="imie"><br />Nazwisko: <input type="text" id="nazwisko"></h1>';
+  tresc+='<h1>Email: <input type="email" id="email"></form>';
+  tresc+='<h1><button onclick="pokazDane()"></h1>';
   return tresc;
 }
 
 function pokazDane(){
-  //Funkcja zbiera dane wpisane w pola formularza i wyświetla okienko
-  //typu confirm do zatwierdzenia przez użytkownika:
   var dane="Następujące dane zostaną wysłane:\n";
+  dane+="Imie:" + document.getElementById('imie').value+"\n";
+  dane+="Nazwisko:" + document.getElementById('nazwisko').value+"\n";
   dane+="Email: "+document.getElementById('email').value+"\n";
-  // uzupełnij dane ...
   if (window.confirm(dane)){ 
     return true;
   } else {
