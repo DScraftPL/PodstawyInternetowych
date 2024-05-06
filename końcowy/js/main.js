e7c9a9af43ff24d3f75b4c81b083912c17104912
DScraftPL <xxxwiacekxxx@gmail.com>
2024-05-06 10:46:20 +0200

    końcowy: js walidacja

--- końcowy/js/main.js
+++ końcowy/js/main.js
@@ -2,11 +2,13 @@ function sprawdz(){
   window.alert("działa");
   var elementRoboczy = document.getElementById("emailaddress");
   var elementError = document.getElementById("emailError");
-  if(elementRoboczy.value != null){
-    elementError.innerHTML = "";
+  var elementRegex = /^([a-zA-Z0-9])+([.a-zA-Z0-9_-])*@([a-zA-Z0-9_-])+(.[a-zA-Z0-9_-]+)+/;
+  if(!elementRegex.test(elementRoboczy.value)){
+    elementError.innerHTML = "Error2";
   } else {
     elementError.innerHTML = "Error";
   }
+  window.alert("sprawdz");
 }
 
 function wyswietl(){
