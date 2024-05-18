ebd581d7de038c9d3af216c73f6a04b24ab52444
DScraftPL <xxxwiacekxxx@gmail.com>
2024-05-18 11:06:49 +0200

    koncowy: uzytkownicy i localstorage!

--- końcowy/js/main.js
+++ końcowy/js/main.js
@@ -57,22 +57,35 @@ function sprawdz(){
 
 function sprawdzwyslij(){
   if(sprawdz()){
+    let indeks = 0;
+    console.log(localStorage.getItem("user" + indeks));
+    while(localStorage.getItem("user" + indeks) != null){
+      console.log(localStorage.getItem("user" + indeks));
+      indeks += 1;
+    }
     dane = "";
     profil = {};
     var dana = document.getElementById("emailaddress").value;
     profil.email = dana; 
-    dane += dana + "\n" ;
+    dane += "email: " + dana + "\n" ;
     dana = document.getElementById("name").value;
     profil.name = dana;
-    dane += dana + "\n";
+    dane += "imie i nazwisko: " + dana + "\n";
     dana = document.getElementById("password").value;
     profil.password = dana;
-    dane += dana + "\n";
+    dane += "haslo: " + dana + "\n";
     dana = document.getElementById("wyborselect").value;
     profil.wybor = dana;
-    dane += dana + "\n";
+    dane += "wybor: " + dana + "\n";
+    var radios = document.getElementsByName("radioInput");
+    for(let i=0; i<radios.length; i++){
+      if(radios[i].checked){
+        profil.radio = i;
+        dane += "radio" + i + "\n";
+      }
+    }
     if(window.confirm(dane)){
-      localStorage.setItem("user", JSON.stringify(profil));
+      localStorage.setItem("user"+indeks, JSON.stringify(profil));
     } 
   } else {
     window.alert("sprawdź formularz");
@@ -80,12 +93,40 @@ function sprawdzwyslij(){
 }
 
 function wyswietl(){
-  for(let i=0; i<localStorage.length; i++){
-    var user = JSON.parse(localStorage.getItem('user'));
-    console.log(user);
-    var element = document.getElementById("user");
-    for(let i=0; i<localStorage.length; i++){
-      element.innerHTML += '<div>' + user.name + '<br>' + user.email + '</div>';
-    }
+  var wewHTML = '<h3 class="mt-2 text-center">Zapisani użytkownicy!</h3>' + '<div class="row">';
+  var found = 0; 
+  var i = 0;
+  console.log(localStorage.length)
+  if(localStorage.length == 0){
+    wewHTML = '<h3 class="mt-2 text-center">Nikt się nie zapisał ;( </h3>';
   }
-} 
+  while(found < localStorage.length){
+    console.log(found);
+    if(localStorage.getItem("user"+i) != null){
+      found += 1;
+      var user = JSON.parse(localStorage.getItem("user" + i));
+      wewHTML += '<div class="col-12 col-md-6 col-xl-3 border border-2 text-center">' + user.name + '<br>' + user.email + '<div class="row"><div class="col-6"><button type="button" class="btn btn-info" onclick="modyfikuj(' + i + ')">Modyfikuj</button></div><div class="col-6"><button type="button" class="btn btn-info" onclick="usun('+ i +')">Usuń</button></div></div>' + '</div>';
+    }
+    i+=1;
+  }
+  wewHTML += '</div>';
+  console.log(wewHTML);
+  var element = document.getElementById("users");
+  element.innerHTML = wewHTML;
+}
+
+function modyfikuj(id){
+  var user = JSON.parse(localStorage.getItem("user" + id));
+  document.getElementById("name").value = user.name;
+  document.getElementById("emailaddress").value = user.email;
+  document.getElementById("password").value = user.password;
+  document.getElementById("wyborselect").value = user.wybor;
+  document.getElementById("EULAcheck").checked = true;
+  var radios = document.getElementsByName("radioInput");
+  radios[user.radio].checked = true;
+}
+
+function usun(id){
+  localStorage.removeItem("user" + id);
+  wyswietl();
+}
