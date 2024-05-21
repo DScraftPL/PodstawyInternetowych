865c7988130bf74cdcfc0e3a1a4c6600ecf8abfd
DScraftPL <xxxwiacekxxx@gmail.com>
2024-05-21 16:47:53 +0200

    końcowy: backend zwracający losowy ciąg znaków i losową liczbę

---
+++ końcowy/js/backend/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const crypto = require('crypto');
+
+app.get('/',(req,res) => {
+  var liczba = Math.floor(Math.random()*255); //od 0 do 10
+  var wysylka = {};
+  wysylka.liczba = liczba;
+  wysylka.ciag = crypto.randomBytes(20).toString('hex');
+  res.send(JSON.stringify(wysylka));
+}) 
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
